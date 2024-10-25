5eff74b37a785e50d8219bb2cae79289ccafe153
mecoxeco <37673813+mecoxeco@users.noreply.github.com>
2024-10-25 13:40:37 -0300

    Exercise 01 - using console.log

---
+++ 01-console.log/01-exercise.js
@@ -0,0 +1,11 @@
+/*
+
+    Ex 01 - Print your name and say that you are learning javascript!
+
+*/
+
+
+const myname = "Otávio Luque"
+
+console.log(`Hello, my name is ${myname} and i am learning JavaScript today!`);
+
---
+++ 01-console.log/index.js
@@ -0,0 +1,13 @@
+// Basic Variable
+let age = 15;
+
+// Basic console.log
+console.log('Hello World!');
+console.log(age);
+console.log(35, 155.5);
+
+// Basic concatenation
+console.log('Your agr is:', age)
+
+//basic interpolation
+console.log(`Your age is ${age}`)
