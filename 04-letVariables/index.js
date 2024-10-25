e8fe846bf8c5bd810537b0148ce3580a772c7ae6
mecoxeco <37673813+mecoxeco@users.noreply.github.com>
2024-10-25 14:35:16 -0300

    04-letVariables

---
+++ 04-letVariables/index.js
@@ -0,0 +1,14 @@
+/*
+    In JS we can use LET to create a block-scoped local variable that is re-assignable
+    we can optionally initialize them with a value or data type
+    
+*/
+
+let charName = 'Ezio'
+var birthYear = 1966;
+let game;
+
+console.log(`${charName} was born in ${birthYear}.`);
+
+game = "Assasin's Creed II"
+console.log(`${charName} appeared on ${game}.`)
