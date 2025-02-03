0dff3415d51f2c01e75e4de084dbf55c5888176f
oluquejs <o.p.luque@hotmail.com>
2025-02-03 16:45:55 -0300

    chore(21.1): add oneHour

--- 2.logic/06-dateObject/index.js
+++ 2.logic/06-dateObject/index.js
@@ -8,6 +8,7 @@ const zeroDate = new Date(0);   // timestamp Unix 01/01/1970
 console.log(date.toString());   // Date as String
 console.log(date.getTime());    // Date in milliseconds
 
+const oneHour = 60 * 60 * 1 * 1000;                  // one hour in milliseconds
 const oneDay = 60 * 60 * 24 * 1000;                  // one day in milliseconds
 const nextDay = new Date(date.getTime() + oneDay);   // next day
 console.log(nextDay.toString())
