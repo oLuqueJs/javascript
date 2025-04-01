3e1aa321744124a6d453bbdc5cbf9a8ba8dd8e0f
oluquejs <luquedevjs@icloud.com>
2025-04-01 17:40:12 -0300

    43 - factoryFunctions

---
+++ 3.functions/08-factoryFunctions/index.js
@@ -0,0 +1,74 @@
+// * Factory Function
+function createPerson(name, surname) {
+  return {
+    name,
+    surname
+  }
+}
+const p1 = createPerson('Luis', 'Pereira')
+console.log(p1) // ! return object = { name: 'Louis', surname: 'Pereira' }
+
+
+// * Factory Function with Methods
+function createPerson2(name, surname) {
+  return {
+    name,
+    surname,
+
+    // ! Method
+    speak: function(subject) {
+      return `${name} is speaking about ${subject}. \n${name} is ${this.mood}`
+    },
+    mood: 'Happy' // ! must use THIS.
+  }
+}
+const p2 = createPerson2('Marcelo', 'Bandeira')
+console.log(p2.speak('JavaScript'))
+
+
+// * Factory Function with Getter
+function createPerson3(name, surname, book) {
+  return {
+    name,
+    surname,
+    bookname: book,
+
+    speak: function(subject) {
+      return `${name} is speaking about ${subject}. \n${name} is ${this.mood}`
+    },
+
+    // ! Getter
+    get bookDetails() {
+      return this.bookname;
+    }
+  }
+}
+const p3 = createPerson3('Rodrigo', 'Formiga', '100 Noites')
+console.log(p3.bookDetails)
+
+
+// * Factory Function with Setter
+function createPerson4(name, surname, book) {
+  return {
+    name,
+    surname,
+    bookname: book,
+
+    speak: function(subject) {
+      return `${name} is speaking about ${subject}. \n${name} is ${this.mood}`
+    },
+
+    get bookDetails() {
+      return this.bookname;
+    },
+
+    // ! Setter
+    set bookDetails(name) {
+      this.bookname = name;
+    }
+
+  }
+}
+const p4 = createPerson4('Paulo', 'Chagas', 'O rei')
+p4.bookDetails = "O louco"
+console.log(p4.bookDetails)
