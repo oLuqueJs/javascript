9aaa78f603c1c5f969ff2cac565e9ed5604f0f58
oluquejs <luquedevjs@icloud.com>
2025-04-14 17:47:56 -0300

    49 - spliceFunction

---
+++ 4.arrays/02-splice/index.js
@@ -0,0 +1,11 @@
+// * Array with Names
+const names = ['Clover', 'Marc', 'John', 'Maria', 'Suy', 'Adam']
+
+// * Splice Function 
+// * Params: (index, deleteQuantity, element 1, element 2...)
+const removed = names.splice(3, 3, 'Robert', 'Kyan', 'Yago')
+
+
+console.log(names)
+console.log(removed)
+
